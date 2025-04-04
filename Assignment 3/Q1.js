2f29c6828b6c9a6f2c5c9af98209feea98008ca2
Mohit Gangawr <mohitgangawr@Mohits-MacBook-Air.local>
2025-04-04 21:40:55 +0530

    fourth commit

---
+++ Assignment 3/Q1.js
@@ -0,0 +1,15 @@
+let states = [
+    "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
+    "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand",
+    "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur",
+    "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab",
+    "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura",
+    "Uttar Pradesh", "Uttarakhand", "West Bengal"
+];
+
+// Use filter to remove states that start with vowels (A, E, I, O, U)
+
+
+let filteredStates = states.filter(state => !/^[AEIOU]/i.test(state));
+
+console.log(filteredStates);
