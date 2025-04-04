ffc76c8cbfdb209e24266f941be9d379228a6a37
Mohit Gangawr <mohitgangawr@Mohits-MacBook-Air.local>
2025-04-04 23:26:12 +0530

    11th commit

---
+++ Assignment 3/Q8.js
@@ -0,0 +1,9 @@
+function repeatedDigitSum(num) {
+    while (num >= 10) {
+        num = num.toString().split("").reduce((sum, digit) => sum + parseInt(digit), 0);
+    }
+    return num;
+}
+
+
+console.log(repeatedDigitSum(456)); // Output: 6
